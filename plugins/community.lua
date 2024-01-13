f29450c43e389ebfe4900662ec6e8f748339c789
Vafil42 <vafil6241@gmail.com>
2024-01-13 12:37:39 +0300

    this is my swamp

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.test.neotest" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
